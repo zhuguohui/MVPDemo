de4baa36b13e5a5e17a86d7323f97e55653209cf
zhuguohui <287718603@qq.com>
2016-07-04 11:27:09 +0800

    整理注释

--- app/src/main/java/com/zgh/mvpdemo/model/news/IListMode.java
+++ app/src/main/java/com/zgh/mvpdemo/model/news/IListMode.java
@@ -1,8 +1,14 @@
 package com.zgh.mvpdemo.model.news;
 
 /**
- * Created by yuelin on 2016/6/29.
+ * Created by zhuguohui on 2016/6/29.
  */
 public interface IListMode {
+    /**
+     *
+     * @param useCache 是否使用缓存
+     * @param url 数据url
+     * @param listener 回调接口
+     */
     void LoadData(boolean useCache,String url,DataResultListener listener);
 }
